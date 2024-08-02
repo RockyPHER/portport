7185f953eec818c5a20bb4a9e6c675076facce0f
RockyPher <davidsonjunior10@gmail.com>
2024-08-02 03:08:19 -0300

    chore: não sei o que eu fiz

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,7 +4,15 @@ export const Contact = () => {
       <div className="w-[30%] h-[30%]">
         <h1 className="text-3xl">Contact</h1>
         <div className="flex flex-col items-start">
-          <p>GitHub:</p>
+          <div className="flex gap-2">
+            <p>GitHub:</p>
+            <a
+              className="text-white hover:text-black"
+              href="https://github.com/RockyPHER"
+            >
+              https://github.com/RockyPHER
+            </a>
+          </div>
           <p>LinkedIn:</p>
           <p>Email:</p>
           <p>Phone:</p>
